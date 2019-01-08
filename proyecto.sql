a03576bb971ea3b06d874521181767695a0ea29d
algargon99 <a.gargon99@gmail.com>
2019-01-08 12:14:46 +0100

    basededatos

---
+++ proyecto.sql
@@ -0,0 +1,168 @@
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: proyecto
+-- ------------------------------------------------------
+-- Server version	5.5.41-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Clientes`
+--
+
+DROP TABLE IF EXISTS `Clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Clientes` (
+  `CodCliente` int(11) NOT NULL AUTO_INCREMENT,
+  `DNI` char(9) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Apellidos` varchar(45) NOT NULL,
+  `Direccion` varchar(255) DEFAULT NULL,
+  `Telefono` int(11) DEFAULT NULL,
+  `Codigo_Postal` char(5) DEFAULT NULL,
+  PRIMARY KEY (`CodCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Clientes`
+--
+
+LOCK TABLES `Clientes` WRITE;
+/*!40000 ALTER TABLE `Clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Empleados`
+--
+
+DROP TABLE IF EXISTS `Empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Empleados` (
+  `CodEmpleado` int(11) NOT NULL,
+  `DNI` char(9) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Apellidos` varchar(45) NOT NULL,
+  `Direccion` varchar(255) DEFAULT NULL,
+  `Telefono` char(9) DEFAULT NULL,
+  `FechaContratacion` date DEFAULT NULL,
+  PRIMARY KEY (`CodEmpleado`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Empleados`
+--
+
+LOCK TABLES `Empleados` WRITE;
+/*!40000 ALTER TABLE `Empleados` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Empleados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Libros`
+--
+
+DROP TABLE IF EXISTS `Libros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Libros` (
+  `ISBN` int(13) NOT NULL AUTO_INCREMENT,
+  `Titulo` varchar(45) NOT NULL,
+  `Autor` varchar(45) NOT NULL,
+  `Editorial` varchar(45) NOT NULL,
+  `NumPag` int(11) DEFAULT NULL,
+  `Encuadernacion` varchar(45) DEFAULT NULL,
+  `Precio` int(11) NOT NULL,
+  PRIMARY KEY (`ISBN`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Libros`
+--
+
+LOCK TABLES `Libros` WRITE;
+/*!40000 ALTER TABLE `Libros` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Libros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Pedidos`
+--
+
+DROP TABLE IF EXISTS `Pedidos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Pedidos` (
+  `CodPedido` int(11) NOT NULL AUTO_INCREMENT,
+  `FechaEntrega` varchar(45) NOT NULL,
+  `CodCliente` int(11) DEFAULT NULL,
+  `CodEmpleado` int(11) DEFAULT NULL,
+  PRIMARY KEY (`CodPedido`),
+  KEY `fk_Pedidos_1_idx` (`CodCliente`),
+  KEY `fk_Pedidos_2_idx` (`CodEmpleado`),
+  CONSTRAINT `fk_Pedidos_1` FOREIGN KEY (`CodCliente`) REFERENCES `Clientes` (`CodCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Pedidos_2` FOREIGN KEY (`CodEmpleado`) REFERENCES `Empleados` (`CodEmpleado`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Pedidos`
+--
+
+LOCK TABLES `Pedidos` WRITE;
+/*!40000 ALTER TABLE `Pedidos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Pedidos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Tienen`
+--
+
+DROP TABLE IF EXISTS `Tienen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Tienen` (
+  `Cantidad` int(11) NOT NULL,
+  `CodPedido` int(11) NOT NULL,
+  `ISBN` int(13) NOT NULL,
+  KEY `fk_Tienen_1_idx` (`ISBN`),
+  KEY `fk_Tienen_2_idx` (`CodPedido`),
+  CONSTRAINT `fk_Tienen_1` FOREIGN KEY (`ISBN`) REFERENCES `Libros` (`ISBN`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Tienen_2` FOREIGN KEY (`CodPedido`) REFERENCES `Pedidos` (`CodPedido`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Tienen`
+--
+
+LOCK TABLES `Tienen` WRITE;
+/*!40000 ALTER TABLE `Tienen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Tienen` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-01-08 11:02:57
+
